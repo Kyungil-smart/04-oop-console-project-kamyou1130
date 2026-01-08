0d34e922814662d637812718003cb3779136dc7c
kamyou <example1130@naver.com>
2026-01-08 19:15:38 +0900

    콘솔 프로젝트 아키텍처 리포지토리 커밋

---
+++ 260107/260107/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        GameManager gameManager = new GameManager();
+        gameManager.Run();
+    }
+}
